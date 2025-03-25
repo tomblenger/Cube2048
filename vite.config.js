31f6ef07f901a3206a2a6e6ced2400240839737a
davidnato <dragon20000330@outlook.com>
2025-03-25 18:51:54 -0500

    delete buffer.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import restart from 'vite-plugin-restart'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
